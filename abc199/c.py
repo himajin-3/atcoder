5c05b132a9fe5413fafcf1621cbce63615087a7e
ken <kengokamiguchi@gmail.com>
2021-04-25 20:07:32 +0900

    abc199

--- abc199/c.py
+++ abc199/c.py
@@ -2,21 +2,24 @@ N = int(input())
 S = input()
 Q = int(input())
 
-Q_row = [0] * Q
-N_Len = N/2
-for i in range(Q):
-    Q_row[i] = list(map(int,input().split()))
-
-for i in range(1,Q-1,1):
-    # Tが１の時
-    if(Q_row[i][0] == 1):
-        tmp_aa = S[int(Q_row[i][2])]
-        tmp_bb = S[Q_row[i][1]]
-        S = tmp_bb + tmp_aa
-        print(S)
-    # Tが２以上の時
-    if(Q_row[i][0] == 2):
-        tmp_a = S[0:int(N_Len+1)]
-        tmp_b = S[int(N_Len+1)::]
-        S = tmp_b + tmp_a
+S = [ss for ss in S]
+flg_flip = False
+for _ in range(Q):
+  T, A, B = map(int, input().split())
+  if T==1:
+    if flg_flip:
+      if A > N:
+        A = A - N
+      else:
+        A = A + N
+      if B > N:
+        B = B - N
+      else:
+        B = B + N
+    S[A-1], S[B-1] = S[B-1], S[A-1]
+  else:
+    flg_flip = not flg_flip
+S = "".join(S)
+if flg_flip:
+  S = S[N:] + S[:N]
 print(S)
